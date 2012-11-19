8f0f98b282e3b2520088a0d177ea6feb96e216e8
Pavel-Teplitsky <asup07@rambler.ru>
2012-11-19 09:41:03 +0200

    update sample7

--- examples/api-samples/inc_samples/sample7.php
+++ examples/api-samples/inc_samples/sample7.php
@@ -6,35 +6,27 @@
     {
         F3::set('userId', $clientId);
         F3::set('privateKey', $privateKey);
-        
         $signer = new GroupDocsRequestSigner($privateKey);
         $apiClient = new APIClient($signer); // PHP SDK V1.1
         $mgmtApi = new MgmtApi($apiClient);
         $userAccountInfo = $mgmtApi->GetUserProfile($clientId);
         $api = new StorageApi($apiClient);
-       // $api->setBasePath("http://localhost:7000/v2.0");
+        $api->setBasePath("http://localhost:7000/v2.0");
         $files = $api->ListEntities($clientId, "", null, null, null, null, null, null, true); //geting all Entities from curent user
         $thumbnail = '';
-        
-//        $fileInfo = $files->result->files[0];
-        $fp = fopen(__DIR__ . '/../temp/thumbnail.jpg', 'w');
-        fwrite($fp, base64_decode($files->result->files[0]->thumbnail));
-        fclose($fp);
-//        var_dump($fp);
-//        for ($i=0; $i < count($files->result->files); $i++)
-//        {
-//            $thumbnailName = $i . '.jpg';
-//            $fp = fopen(__DIR__ . '/../temp/' . $thumbnailName, 'w');
-//            fwrite($fp, base64_decode($files->result->files[$i]->thumbnail));
-//            fclose($fp);
-//        }
-        foreach ($files->result->files as $item) //selecting file names
+        $name = '';
+        for ($i=0; $i < count($files->result->files); $i++)
         {
-            $thumbnail .= '<img src="' . __DIR__ . '/../temp/thumbnail.jpg">' . $item->name . '<br>';
+            if($files->result->files[$i]->thumbnail !== "")
+            {
+            $fp = fopen(__DIR__ . '/../temp/thumbnail' . $i . '.jpg', 'w');
+            fwrite($fp, base64_decode($files->result->files[$i]->thumbnail));
+            fclose($fp);
+            $thumbnail .= '<img src= "/temp/thumbnail' . $i . '.jpg", width="40px", height="40px">'
+                          .$name = $files->result->files[$i]->name . '</img> <br>';
+            $name = $files->result->files[$i]->name;
+            }            
         }
-           
         F3::set('thumbnailList', $thumbnail);
-
     }
-    
     echo Template::serve('sample7.htm');

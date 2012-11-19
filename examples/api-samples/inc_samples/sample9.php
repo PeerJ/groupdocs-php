08a0b4119877c1275b70c9b2718fc8275e8c4360
Pavel-Teplitsky <asup07@rambler.ru>
2012-11-19 14:57:44 +0200

    update sample9

--- examples/api-samples/inc_samples/sample9.php
+++ examples/api-samples/inc_samples/sample9.php
@@ -5,12 +5,34 @@
     $clientId = F3::get('POST["client_id"]');
     $privateKey = F3::get('POST["private_key"]');
     $fileGuId = f3::get('POST["fileId"]');
-    if (isset($clientId) AND isset($privateKey))
+    $width = f3::get('POST["width"]');
+    $height = f3::get('POST["height"]');
+    
+    function Iframe($file_Id, $width='400', $height='650'){
+        var_dump($file_Id);
+        if ($file_Id == "")
     {
-        $iframe = 'https://apps.groupdocs.com/document-viewer/embed/' . $fileGuId . '?frameborder="0" width="600" height="400"';
-        f3::set('url', $iframe);
-        F3::set('userId', $clientId);
-        F3::set('privateKey', $privateKey);
+        throw new Exception('You do not enter FILE ID');
+    } 
+    else
+    {
+        $iframe = 'https://apps.groupdocs.com/document-viewer/embed/' . $file_Id . '?frameborder="0" width="'.$width.'" height="'.$height.'"';
+        return f3::set('url', $iframe);;
+    }
     }
     
+    try 
+    {
+        Iframe($fileGuId, $width, $height);
+    } 
+    catch (Exception $e)
+    {
+        $error = 'ERROR: ' .  $e->getMessage() . "\n";
+        f3::set('error', $error);
+    }
+    F3::set('userId', $clientId);
+    F3::set('privateKey', $privateKey);
+    F3::set('width', $width);
+    F3::set('height', $height);
+    f3::set('fileId', $fileGuId);
     echo Template::serve('sample9.htm');

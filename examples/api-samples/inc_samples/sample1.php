98e8607178f0c70c18d030389acc8500d4e8c051
Sergey Zhuravel <sergey.zhuravel@groupdocs.com>
2012-12-20 09:47:50 +0200

    Updated composer.phar. Updated

--- examples/api-samples/inc_samples/sample1.php
+++ examples/api-samples/inc_samples/sample1.php
@@ -1,5 +1,7 @@
 <?php
+//<i>This sample will show how to use <b>Signer object</b> to be authorized at GroupDocs and how to get GroupDocs user infromation using PHP SDK</i>
 
+//###Set variables and get POST data
     F3::set('userId', '');
     F3::set('privateKey', '');
     F3::set('fileId', '');
@@ -13,13 +15,22 @@
         } else {
             F3::set('userId', $clientId);
             F3::set('privateKey', $privateKey);
-
+            //###Create Signer, ApiClient and Management Api objects
+            
+            //Create signer object
             $signer = new GroupDocsRequestSigner($privateKey);
-            $apiClient = new APIClient($signer); // PHP SDK V1.1
-
+            //Create apiClient object
+            $apiClient = new APIClient($signer); // PHP SDK Version > 1.0
+            
+            //Create Management Api object
             $mgmtApi = new MgmtApi($apiClient);
+            
+            //###Make a request to Management API using clientId
             $userAccountInfo = $mgmtApi->GetUserProfile($clientId);
+            
+            //Check the result of the request
             if (isset($userAccountInfo->result) AND isset($userAccountInfo->result->user)) {
+                //If request was successfull - set userInfo variable for template
                 return F3::set('userInfo', $userAccountInfo->result->user);
             }
         }
@@ -31,4 +42,5 @@
         $error = 'ERROR: ' .  $e->getMessage() . "\n";
         f3::set('error', $error);
     }
+    //Process template
     echo Template::serve('sample1.htm');

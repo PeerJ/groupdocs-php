fbfee8148c12ddce15e80f2daa1ed4c1cd835d18
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2012-12-24 11:38:09 +0200

    added comments

--- examples/api-samples/inc_samples/sample2.php
+++ examples/api-samples/inc_samples/sample2.php
@@ -1,4 +1,7 @@
 <?php
+//<i>This sample will show how to use <b>ListEntities</b> to list files within GroupDocs Storage using the Storage API</i>
+
+//###Set variables and get POST data
 
     $clientId = F3::get('POST["client_id"]');
     $privateKey = F3::get('POST["private_key"]');
@@ -8,17 +11,27 @@
         if (empty($clientId) || empty($privateKey)) {
             throw new Exception('Please enter all required parameters');
         } else {
-            $signer = new GroupDocsRequestSigner($privateKey);
-            $apiClient = new APIClient($signer); // PHP SDK V1.1
-            $api = new StorageApi($apiClient);
-            $files = $api->ListEntities($clientId, '', 0); //geting all Entities from curent user
-            $name = '';
+             //###Create Signer, ApiClient and Management Api objects
             
+            //Create signer object
+            $signer = new GroupDocsRequestSigner($privateKey);
+            //Create apiClient object
+            $apiClient = new APIClient($signer); // PHP SDK V1.1
+            //Create api object
+            $api = new StorageApi($apiClient);
+            
+            //###Make a request to Storage API using clientId
+            
+            //geting all Entities from curent user
+            $files = $api->ListEntities($clientId, '', 0); 
+                        
             //selecting file names
+            $name = '';
             foreach ($files->result->files as $item) {
                 $name .= $item->name . '<br>';
             }
-
+            
+           //Returning file names to the Viewer
            return F3::set('filelist', $name);
         }
     }
@@ -30,6 +43,7 @@
         f3::set('error', $error);
     }
     
+    //Process template
     F3::set('userId', $clientId);
     F3::set('privateKey', $privateKey);
     echo Template::serve('sample2.htm');

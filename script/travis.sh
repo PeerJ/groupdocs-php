9902705988ab5346ba28c6cd4c5a31b306e03268
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2013-01-11 16:01:04 +0200

    Update script/travis.sh

--- script/travis.sh
+++ script/travis.sh
@@ -5,4 +5,4 @@ phpunit
 
 # integration tests
 cd tests/samples-ui-tests
-mvn -q -s settings.xml clean test
+mvn -q -s settings.xml clean test -Dsite.url="http://groupdocs-php-samples.herokuapp.com/"

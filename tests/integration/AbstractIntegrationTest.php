1316ad0e9094174279dce39c12c3f1730c5a9c47
Zhaidarbek Ayazbayev <zhaidarbek@gmail.com>
2013-01-30 10:46:02 +0600

    fix failing test

--- tests/integration/AbstractIntegrationTest.php
+++ tests/integration/AbstractIntegrationTest.php
@@ -24,4 +24,8 @@ class AbstractIntegrationTest extends PHPUnit_Framework_TestCase {
 		$this->userId = "a9f81d75a3a7df86";
 	}
 	
+	public function testDummy(){
+		assertThat(true, notNullValue());
+	}
+	
 }

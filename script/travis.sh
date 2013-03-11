5a025e4264201639e301c68b2964692ca93e3b74
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2013-03-11 08:30:58 +0200

    Update travis.sh

--- script/travis.sh
+++ script/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
 # integration tests
-cd tests/samples-ui-tests
-mvn -q -s settings.xml clean test -Dsite.url="http://groupdocs-php-samples.herokuapp.com/"
+#cd tests/samples-ui-tests
+#mvn -q -s settings.xml clean test -Dsite.url="http://groupdocs-php-samples.herokuapp.com/"

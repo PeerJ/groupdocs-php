7ceaef74d87791c2812ce6895ec94a8c317d3651
proger <3skif9@mail.ru>
2014-03-13 08:28:16 +0200

    Swap a samples 42 and 43

--- examples/api-samples/inc_samples/sample43.php
+++ examples/api-samples/inc_samples/sample43.php
@@ -1,62 +1,105 @@
 <?php
 
-//###This sample will show how to download document with annotations using PHP SDK
-// Set variables and get POST data
+//###<i>This sample will show how to add numeration in the doc file</i>
+//Set variables and get POST data
 F3::set('userId', '');
 F3::set('privateKey', '');
-F3::set('fileId', '');
-$clientId = F3::get('POST["clientId"]');
-$privateKey = F3::get('POST["privateKey"]');
-$fileId = F3::get('POST["fileId"]');
-if (empty($clientId) || empty($privateKey) || empty($fileId)) {
-    $error = 'Please enter all required parameters';
-    F3::set('error', $error);
-} else {
-    //Get base path
-    $basePath = F3::get('POST["basePath"]');
-    F3::set('userId', $clientId);
-    F3::set('privateKey', $privateKey);
-    F3::set('fileId', $fileId);
 
-    #### Create Signer, ApiClient and Annotation Api objects
-    # Create signer object
-    $signer = new GroupDocsRequestSigner($privateKey);
-    # Create apiClient object
-    $apiClient = new ApiClient($signer);
-    # Create Annotation object
-    $antApi = new AntApi($apiClient);
-    //Create AsyncApi object
-    $asyncApi = new AsyncApi($apiClient);
-    //Create SharedApi object
-    $sharedApi = new SharedApi($apiClient);
-    if ($basePath == "") {
-        //If base base is empty seting base path to prod server
-        $basePath = 'https://api.groupdocs.com/v2.0';
-    }
-    //Set base path
-    $antApi->setBasePath($basePath);
-    $asyncApi->setBasePath($basePath);
-    $sharedApi->setBasePath($basePath);
-    # Make a request to Annotation API using clientId and fileId
-    try {
-        $list = $antApi->ListAnnotations($clientId, $fileId);
-        if ($list->status == "Ok") {
-            // Check the result of the request
-            if (isset($list->result) and !empty($list->result->annotations) ) {
-                //Create job info object
+$clientID = F3::get('POST["clientId"]');
+$privateKey = F3::get('POST["privateKey"]');
+$basePath = F3::get('POST["basePath"]');
+
+try {
+    //###Check if user entered all parameters
+    if (empty($clientID) || empty($privateKey)) {
+        throw new Exception('Please enter FILE ID');
+    } else {
+        F3::set('userId', $clientID);
+        F3::set('privateKey', $privateKey);
+        //###Create Signer, ApiClient and Storage Api objects
+        //Create signer object
+        $signer = new GroupDocsRequestSigner($privateKey);
+        //Create apiClient object
+        $apiClient = new APIClient($signer);
+        //Create Doc Api object
+        $docApi = new DocApi($apiClient);
+        //Create Storage Api object
+        $storageApi = new StorageApi($apiClient);
+        //Create AsyncApi object
+        $asyncApi = new AsyncApi($apiClient);
+        $mergApi = new MergeApi($apiClient);
+        //Set url to choose whot server to use
+        if ($basePath == "") {
+            //If base base is empty seting base path to prod server
+            $basePath = 'https://api.groupdocs.com/v2.0';
+        }
+        //Set base path
+        $docApi->setBasePath($basePath);
+        $storageApi->setBasePath($basePath);
+        $asyncApi->setBasePath($basePath);
+        $mergApi->setBasePath($basePath);
+        //Get entered by user data
+        $url = F3::get('POST["url"]');
+        $file = $_FILES['file'];
+        $fileId = F3::get('POST["fileId"]');
+        //Check if user choose upload file from URL
+        if ($url != "") {
+            //Upload file from URL
+            $uploadResult = $storageApi->UploadWeb($clientID, $url);
+            //Check is file uploaded
+            if ($uploadResult->status == "Ok") {
+                //Get file GUID
+                $fileGuId = $uploadResult->result->guid;
+                $fileId = "";
+                //If it isn't uploaded throw exception to template
+            } else {
+                throw new Exception($uploadResult->error_message);
+            }
+        }
+        //Check is user choose upload local file
+        if ($_FILES['file']["name"] != "") {
+            //Temp name of the file
+            $tmpName = $file['tmp_name'];
+            //Original name of the file
+            $name = $file['name'];
+            //Create file stream
+            $fs = FileStream::fromFile($tmpName);
+            //###Make a request to Storage API using clientId
+            //Upload file to current user storage
+            $uploadResult = $storageApi->Upload($clientID, $name, 'uploaded', "", $fs);
+
+            //###Check if file uploaded successfully
+            if ($uploadResult->status == "Ok") {
+                //Get file GUID
+                $fileGuId = $uploadResult->result->guid;
+                $fileId = "";
+
+                //If it isn't uploaded throw exception to template
+            } else {
+                throw new Exception($uploadResult->error_message);
+            }
+        }
+        //Check is user choose file GUID
+        if ($fileId != "") {
+            //Get entered by user file GUID
+            $fileGuId = $fileId;
+        }
+        F3::set('fileId', $fileGuId);
+         //Create job info object
                 $jobInfo = new JobInfo();
-                $jobInfo->actions = "512";
+                $jobInfo->actions = "8193";
+                $jobInfo->out_formats = array('doc');
 
                 //Create new job
-                $createJob = $asyncApi->CreateJob($clientId, $jobInfo);
+                $createJob = $asyncApi->CreateJob($clientID, $jobInfo);
 
                 if ($createJob->status == "Ok") {
                     try {
                         //Add document to job
-                        $addJobDocument = $asyncApi->AddJobDocument($clientId, $createJob->result->job_id, $fileId, false);
-                        if ($addJobDocument->status != "Ok") {
-                            throw new Exception($addJobDocument->error_message);
-                        }
+                            $addJobDocument = $asyncApi->AddJobDocument($clientID, $createJob->result->job_id, $fileGuId, false);
+                            if ($addJobDocument->status != "Ok") {
+                                throw new Exception($addJobDocument->error_message);
+                            }
 
                     } catch (Exception $e) {
                         $error = 'ERROR: ' . $e->getMessage() . "\n";
@@ -66,44 +109,28 @@ if (empty($clientId) || empty($privateKey) || empty($fileId)) {
                         //Change job status
                         $jobInfo->status = 0;
                         //Update job with new status
-                        $updateJob = $asyncApi->UpdateJob($clientId, $createJob->result->job_id, $jobInfo);
+                        $updateJob = $asyncApi->UpdateJob($clientID, $createJob->result->job_id, $jobInfo);
                         if ($updateJob->status == "Ok") {
                             try {
                                 //Delay for server proccesing
                                 sleep(8);
                                 //Get result document guid from job
-                                $getJobDocument = $asyncApi->GetJobDocuments($clientId, $createJob->result->job_id);
+                                $getJobDocument = $asyncApi->GetJobDocuments($clientID, $createJob->result->job_id);
                                 if ($getJobDocument->status == "Ok") {
-                                    // Get guid output file
-                                    $fileGuid = $getJobDocument->result->inputs[0]->outputs[0]->guid;
-                                    // Get name output file
-                                    $fileName = $getJobDocument->result->inputs[0]->outputs[0]->document_path;
                                     //Generate iframe url
                                     if ($basePath == "https://api.groupdocs.com/v2.0") {
-                                        $iframe = 'https://apps.groupdocs.com/document-annotation/embed/' . $getJobDocument->result->inputs[0]->guid;
+                                        $iframe = 'https://apps.groupdocs.com/document-viewer/embed/' . $getJobDocument->result->inputs[0]->outputs[0]->guid;
                                         //iframe to dev server
                                     } elseif ($basePath == "https://dev-api.groupdocs.com/v2.0") {
-                                        $iframe = 'https://dev-apps.groupdocs.com/document-annotation/embed/' . $getJobDocument->result->inputs[0]->guid;
+                                        $iframe = 'https://dev-apps.groupdocs.com/document-viewer/embed/' . $getJobDocument->result->inputs[0]->outputs[0]->guid;
                                         //iframe to test server
                                     } elseif ($basePath == "https://stage-api-groupdocs.dynabic.com/v2.0") {
-                                        $iframe = 'https://stage-apps-groupdocs.dynabic.com/document-annotation/embed/' . $getJobDocument->result->inputs[0]->guid;
+                                        $iframe = 'https://stage-apps-groupdocs.dynabic.com/document-viewer/embed/' . $getJobDocument->result->inputs[0]->outputs[0]->guid;
                                     } elseif ($basePath == "http://realtime-api.groupdocs.com") {
-                                        $iframe = 'https://relatime-apps.groupdocs.com/document-annotation/embed/' . $getJobDocument->result->inputs[0]->guid;
+                                        $iframe = 'https://relatime-apps.groupdocs.com/document-viewer/embed/' . $getJobDocument->result->inputs[0]->outputs[0]->guid;
                                     }
                                     $iframe = $signer->signUrl($iframe);
                                     F3::set('url', $iframe);
-                                    //Local path to the downloads folder
-                                    $downloadFolder = dirname(__FILE__) . '/../downloads';
-                                    //Check is folder exist
-                                    if (!file_exists($downloadFolder)) {
-                                        //If folder don't exist create it
-                                        mkdir($downloadFolder);
-                                    }
-                                    //Obtaining file stream of downloading file and definition of folder where to download file
-                                    $outFileStream = FileStream::fromHttp($downloadFolder, $fileName);
-                                    //Download file from GroupDocs.
-                                    $downloadDocument = $sharedApi->Download($fileGuid, $fileName, null, $outFileStream);
-                                    F3::set("message", "<span style=\"color:green\">File with annotations was downloaded to server's local folder. You can check them <a href=\"/downloads/{$fileName}\">here</a></span>");
                                 } else {
                                     throw new Exception($getJobDocument->error_message);
                                 }
@@ -121,18 +148,10 @@ if (empty($clientId) || empty($privateKey) || empty($fileId)) {
                 } else {
                     throw new Exception($createJob->error_message);
                 }
-
-            } else {
-                throw new Exception("Your file has no annotations");
-            }
-
-        } else {
-            throw new Exception($list->error_message);
-        }
-    } catch (Exception $e) {
-        $error = 'ERROR: ' . $e->getMessage() . "\n";
-        F3::set('error', $error);
     }
+} catch (Exception $e) {
+    $error = 'ERROR: ' . $e->getMessage() . "\n";
+    F3::set('error', $error);
 }
-// Process template
+//Process template
 echo Template::serve('sample43.htm');

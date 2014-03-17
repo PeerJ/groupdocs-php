019620d85e0298d7866ec3d61817999222fc1ff3
proger <3skif9@mail.ru>
2014-03-17 11:00:36 +0200

    fixed bugs in sample41.htm

--- examples/api-samples/inc_samples/callbacks/sample41_callback.php
+++ examples/api-samples/inc_samples/callbacks/sample41_callback.php
@@ -8,12 +8,15 @@ $privateKey = trim($userInfo[1]);
 $guid = null;
 //Get raw data
 $json = file_get_contents("php://input");
+
 //path to settings file - temporary save userId and apiKey like to property file
 //Decode json with raw data to array
 $callBack_data = json_decode($json, true);
 $serializedData = json_decode($callBack_data['SerializedData'], true);
+
 $documentGuid = $serializedData['DocumentGuid'];
 $collaboratorGuid = $serializedData['UserGuid'];
+
 if ($documentGuid != "" && $collaboratorGuid != '') {
     //Create signer object
     $signer = new GroupDocsRequestSigner(trim($privateKey));
@@ -22,26 +25,27 @@ if ($documentGuid != "" && $collaboratorGuid != '') {
     //Create AsyncApi object
     $antApi = new AntApi($apiClient);
     //Get all collaborators for current document
-    $getCollaborators = $antApi->GetAnnotationCollaborators($clientId, $documentGuid);
-    if ($getCollaborators->status == "Ok") {
+    $getCollaborators = $antApi->GetAnnotationCollaborators($clientId, $documentGuid);   
+	if ($getCollaborators->status == "Ok") {
         //Create ReviewerInfo array
         $reviewer = new ReviewerInfo ();
-        //Loop for checking all collaborators
-        for ($n = 0; $n < count($getCollaborators->result->collaborators); $n++) {
-            //Set reviewer rights to view only
-            if ($getCollaborators->result->collaborators[$n]->guid == $collaboratorGuid) {
+        //Loop for checking all collaborators        
+		for ($n = 0; $n < count($getCollaborators->result->collaborators); $n++) {
+			//Set reviewer rights to view only
+			if ($getCollaborators->result->collaborators[$n]->guid == $collaboratorGuid) {
                 //Add riviewer to ReviewerInfo array
                 $reviewer ->id = $getCollaborators->result->collaborators[$n]->id;
                 $reviewer ->access_rights = 1;
-                $reviewers = array($reviewer);
+                $reviewer = array($reviewer);
             }
-        }
-
-        $setReviewer = $antApi->SetReviewerRights($clientId, $documentGuid, $reviewers);
+		}
+		
+        $setReviewer = $antApi->SetReviewerRights($clientId, $documentGuid, $reviewer);
+		
     }
     //path to settings file - temporary save signed document GUID like to property file
     $infoFile = fopen(__DIR__ . '/../../callback_info.txt', 'w');
     fwrite($infoFile, "User rights was set to view only");
     fclose($infoFile);
 }
-
+ 

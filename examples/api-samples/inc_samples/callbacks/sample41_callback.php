7a86671b714d7f93167bf415e35dd1c5aad9e849
proger <3skif9@mail.ru>
2014-03-17 08:45:36 +0200

    fixed bug in file sample41_callback.php

--- examples/api-samples/inc_samples/callbacks/sample41_callback.php
+++ examples/api-samples/inc_samples/callbacks/sample41_callback.php
@@ -12,8 +12,8 @@ $json = file_get_contents("php://input");
 //Decode json with raw data to array
 $callBack_data = json_decode($json, true);
 $serializedData = json_decode($callBack_data['SerializedData'], true);
-$documentGuid = $serializedData['documentGuid'];
-$collaboratorGuid = $serializedData['collaboratorGuid'];
+$documentGuid = $serializedData['DocumentGuid'];
+$collaboratorGuid = $serializedData['UserGuid'];
 if ($documentGuid != "" && $collaboratorGuid != '') {
     //Create signer object
     $signer = new GroupDocsRequestSigner(trim($privateKey));
@@ -24,22 +24,24 @@ if ($documentGuid != "" && $collaboratorGuid != '') {
     //Get all collaborators for current document
     $getCollaborators = $antApi->GetAnnotationCollaborators($clientId, $documentGuid);
     if ($getCollaborators->status == "Ok") {
-
         //Create ReviewerInfo array
         $reviewer = new ReviewerInfo ();
         //Loop for checking all collaborators
         for ($n = 0; $n < count($getCollaborators->result->collaborators); $n++) {
             //Set reviewer rights to view only
-            $getCollaborators->result->collaborators[$n]->access_rights = 'CanView';
-            //Add riviewer to ReviewerInfo array
-            $reviewer[$n] = $getCollaborators->result->collaborators[$n];
-
+            if ($getCollaborators->result->collaborators[$n]->guid == $collaboratorGuid) {
+                //Add riviewer to ReviewerInfo array
+                $reviewer ->id = $getCollaborators->result->collaborators[$n]->id;
+                $reviewer ->access_rights = 1;
+                $reviewers = array($reviewer);
+            }
         }
 
-        $setReviewer = $antApi->SetReviewerRights($clientId, $documentGuid, $reviewer);
+        $setReviewer = $antApi->SetReviewerRights($clientId, $documentGuid, $reviewers);
     }
     //path to settings file - temporary save signed document GUID like to property file
     $infoFile = fopen(__DIR__ . '/../../callback_info.txt', 'w');
     fwrite($infoFile, "User rights was set to view only");
     fclose($infoFile);
 }
+
